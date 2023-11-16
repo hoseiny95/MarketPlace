a1f0d68f9ba69e45010245aaed2e6781637386b3
masi <hoseiny.zp@gmail.com>
2023-11-16 16:07:11 +0330

    Add Seller area

---
+++ App.Endpoints.MVC/Areas/Seller/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Endpoints.MVC.Areas.Seller.Controllers
+{
+    [Area("Seller")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ App.Endpoints.MVC/Areas/Seller/ViewComponents/SidebarComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Endpoints.MVC.Areas.Seller.ViewComponents
+{
+    [Area("Seller")]
+    [ViewComponent(Name = "SidebarComponent")]
+    public class SidebarComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return await Task.FromResult((IViewComponentResult)View("Sidebar"));
+        }
+    }
+}
